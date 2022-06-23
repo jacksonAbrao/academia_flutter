b9e426f3c42896442de8f45b0ac96c20875ec588
jacksonAbrao <jackson.comp1681@hotmail.com>
2022-06-23 00:05:39 -0300

    alt

---
+++ lib/2_modificadores/modificadores.dart
@@ -0,0 +1,6 @@
+void main() {
+  final nomeCompletoFinal = 'Jackson Abrão';
+
+  const nomeCompletoConst =
+      'Teste'; //const tem que ser definido no momento de copilação ->tem o melhor funcionamento
+}
